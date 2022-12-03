d4492bed00a6278341c5f6fa188ca86004a82dbd
Ali Muhammad Moaiz <alimuhammad.moaiz@gmail.com>
2022-12-03 20:49:37 +0500

    Service implemented

---
+++ k190324_Q1/k190324_Q1/Service1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace k190324_Q1
+{
+    [RunInstaller(true)]
+    public partial class Service1 : ServiceBase
+    {
+        int interval = 5;
+        public Thread thread = null;
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            try
+            {
+                ThreadStart start = new ThreadStart(downloading);
+                thread = new Thread(start);
+                thread.Start();
+            }catch (Exception){
+                throw;
+            }
+        }
+
+        protected override void OnStop()
+        {
+            try
+            {
+                if ((thread != null) & thread.IsAlive)
+                    thread.Abort();   
+            }catch (Exception){
+                throw;
+            }
+        }
+
+        public void downloading()
+        {
+            while (true)
+            {
+                using (Process downloadFile = new Process())
+                {
+                    string path = ConfigurationSettings.AppSettings["path"];
+                    downloadFile.StartInfo.UseShellExecute = false;
+                    downloadFile.StartInfo.FileName = path;
+                    downloadFile.StartInfo.CreateNoWindow = true;
+                    downloadFile.Start();
+                }
+                Thread.Sleep(interval * 60 * 1000);     //task will be executed every 5 minutes
+            }
+        }
+    }
+}
---
+++ k190324_Q2/k190324_Q2/Service1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+using ParseDLL;
+
+namespace k190324_Q2
+{
+    public partial class Service1 : ServiceBase
+    {
+        int interval = 10;
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        public void OnTimer(object sender, ElapsedEventArgs args)
+        {
+            parsing();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            System.Timers.Timer timer = new System.Timers.Timer();
+            timer.Interval = interval * 60 * 1000; // 10 minutes
+            timer.Elapsed += new ElapsedEventHandler(this.OnTimer);
+            timer.Start();
+        }
+
+        protected override void OnStop()
+        {
+            
+        }
+
+        public void parsing()
+        {
+            while (true)
+            {
+                HtmlFileManipulation.ParseData();
+                Thread.Sleep(interval * 60 * 1000);     //task will be executed every 10 minutes
+            }
+        }
+    }
+}
